ab9b62a3f27a7855fb3fb96f51a520583f07f826
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2023-04-23 04:43:38 +0530

    modified the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,11 @@ const Header = () => {
         setShowNav(!showNav)
     }
 
+    // Hide nav on button click
+    const handleButtonClick = () => {
+        setShowNav(false)
+    }
+
     const ref = useRef(null)
     useEffect(() => {
         function handleClickOutside(event) {
@@ -26,6 +31,13 @@ const Header = () => {
         };
     }, [ref])
 
+    // PreLogin Links
+    const preLoginLinks = [
+        { link: '#findtalent', title: 'Find Talent'},
+        { link: '#findwork', title: 'Find Work'},
+        { link: '#whycreo', title: 'Why Creo?'},
+    ]
+
     return (
         <header className='py-6 3xl:py-12 relative z-50'>
             <nav className='container flex items-center max-lg:justify-between'>
@@ -34,9 +46,11 @@ const Header = () => {
                 </Link>
                 <div className='hidden lg:flex items-center justify-between w-full'>
                     <ul className='pl-10 space-x-6'>
-                        <a href="#findtalent">Find Talent</a>
-                        <a href="#">Find Work</a>
-                        <a href="#">Why Creo?</a>
+                        {preLoginLinks.map((item,index) => {
+                            return(
+                                <a key={index} href={item.link}>{item.title}</a>
+                            )
+                        })}
                     </ul>
                     <div className='space-x-4'>
                         <Link to='/signup'><button className='primary w-32 h-11'>Sign Up</button></Link>
@@ -47,6 +61,7 @@ const Header = () => {
                 <FiMenu onClick={handleNav} className='max-lg:block hidden text-3xl'/>
             </nav>
 
+            {/* Resounsive View */}
             <div ref={ref} className={`absolute left-0 w-full h-[50vh] shadow-2xl ${showNav === true ? 'top-0' : 'top-[-50vh]'} transition-all duration-300`} style={{ backgroundColor: 'rgba(165, 20, 252, 0.2)', backdropFilter: 'blur(18px)' }}>
                 <div className='py-6 container'>
                     <div className='w-full flex items-center justify-between'>
@@ -56,13 +71,15 @@ const Header = () => {
 
                     <div className='grid justify-items-center gap-6 mt-12'>
                         <ul className='grid justify-items-center space-y-6'>
-                            <a href="#">Find Talent</a>
-                            <a href="#">Find Work</a>
-                            <a href="#">Why Creo?</a>
+                            {preLoginLinks.map((item, index) => {
+                                return (
+                                    <a onClick={handleButtonClick} key={index} href={item.link}>{item.title}</a>
+                                )
+                            })}
                         </ul>
                         <div className='space-x-4'>
-                            <button className='primary w-32 h-11'>Sign Up</button>
-                            <button className='secondary before:content-["Log_In"] w-32 h-11'>Log In</button>
+                            <Link to='/signup'><button onClick={handleButtonClick} className='primary w-32 h-11'>Sign Up</button></Link>
+                            <Link to='/login'><button onClick={handleButtonClick} className='secondary before:content-["Log_In"] w-32 h-11'>Log In</button></Link>
                         </div>
                     </div>
                 </div>
